5c70a066ad6fb6e57dc63e2b53fadf50b174a4b4
Corné Veldman <cveldman@hotmail.com>
2020-06-24 20:44:15 +0200

    Update

--- src/SearchServiceProvider.php
+++ src/SearchServiceProvider.php
@@ -14,30 +14,16 @@ class SearchServiceProvider extends ServiceProvider
         Blade::include('search::default', 'search');
 
         \Illuminate\Database\Eloquent\Builder::macro('search', function ($fields, $key = 'search') {
-
             if(request()->exists($key)) {
-                for($i = 0; $i < count($fields); $i++) {
-                    if($i == 0) {
-                        $this->where($fields[$i], 'like', '%' . request()->get($key) . '%');
-                    } else {
-                        $this->orWhere($fields[$i], 'like', '%' . request()->get($key) . '%');
-                    }
-                }
+                $this->where($fields, 'like', '%' . request()->get($key) . '%');
             }
 
             return $this;
         });
 
         \Illuminate\Database\Query\Builder::macro('search', function ($fields, $key = 'search') {
-
             if(request()->exists($key)) {
-                for($i = 0; $i < count($fields); $i++) {
-                    if($i == 0) {
-                        $this->where($fields[$i], 'like', '%' . request()->get($key) . '%');
-                    } else {
-                        $this->orWhere($fields[$i], 'like', '%' . request()->get($key) . '%');
-                    }
-                }
+                $this->where($fields, 'like', '%' . request()->get($key) . '%');
             }
 
             return $this;
