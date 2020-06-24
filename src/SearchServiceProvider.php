e1f37eb101a5d4415f6e8fd3cedd7dfcc6e243d9
Corné Veldman <cveldman@hotmail.com>
2020-06-24 17:43:33 +0200

    Update

--- src/SearchServiceProvider.php
+++ src/SearchServiceProvider.php
@@ -6,11 +6,6 @@ use Illuminate\Support\ServiceProvider;
 
 class SearchServiceProvider extends ServiceProvider
 {
-    public function register()
-    {
-        //
-    }
-
     public function boot()
     {
         \Illuminate\Database\Eloquent\Builder::macro('search', function ($fields, $key = 'search') {
