54d772da088e4520fd8804a7a47ecb6c94e8948e
Corné Veldman <cveldman@hotmail.com>
2020-06-24 17:28:25 +0200

    Update

---
+++ src/SearchServiceProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Veldman\Dashboard\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class SearchServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        \Illuminate\Database\Eloquent\Builder::macro('search', function ($fields, $key = 'search') {
+
+            if(request()->exists($key)) {
+                for($i = 0; $i < count($fields); $i++) {
+                    if($i == 0) {
+                        $this->where($fields[$i], 'like', '%' . request()->get($key) . '%');
+                    } else {
+                        $this->orWhere($fields[$i], 'like', '%' . request()->get($key) . '%');
+                    }
+                }
+            }
+
+            return $this;
+        });
+
+        \Illuminate\Database\Query\Builder::macro('search', function ($fields, $key = 'search') {
+
+            if(request()->exists($key)) {
+                for($i = 0; $i < count($fields); $i++) {
+                    if($i == 0) {
+                        $this->where($fields[$i], 'like', '%' . request()->get($key) . '%');
+                    } else {
+                        $this->orWhere($fields[$i], 'like', '%' . request()->get($key) . '%');
+                    }
+                }
+            }
+
+            return $this;
+        });
+    }
+}
