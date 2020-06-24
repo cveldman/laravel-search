f082ed7c513c03e5d155dcabe98152709f26b7cc
Corné Veldman <cveldman@hotmail.com>
2020-06-24 19:07:07 +0200

    Update

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -1,5 +1,5 @@
 <form method="get">
-    <div class="input-group mb-3 col-4" style="padding-left: 0; padding-right: 0;">
+    <div class="input-group" style="padding-left: 0; padding-right: 0;">
         <div class="input-group-prepend">
             <span class="input-group-text cil-search" style="border-right: none; background: white;"></span>
         </div>
