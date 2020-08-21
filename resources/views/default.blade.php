15c371686412ca9859fc96f2b8f614e80c5835d9
Corné Veldman <cveldman@hotmail.com>
2020-08-21 13:41:24 +0200

    Update default.blade.php

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -3,6 +3,13 @@
         <div class="input-group-prepend">
             <span class="input-group-text cil-search" style="border-right: none; background: white;"></span>
         </div>
+
+        @foreach(request()->input() as $key => $value)
+            @if($key != "search")
+                <input type="hidden" name="{{ $key }}" value="{{ $value }}">
+            @endif
+        @endforeach
+
         <input type="text" class="form-control" name="search" value="{{ old('search', request()->query('search')) }}" style="border-left: none;">
     </div>
 </form>
